ee49b425decb9d86f10d8b71e564b58e4b17c620
Gabriel Sabella <gabe.sabella.dev@gmail.com>
2024-09-10 19:20:03 -0400

    bulk commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-green-500">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
